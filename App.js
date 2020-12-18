366364fe3bf3b42557b54027e32e938616f9697f
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-12-18 18:10:26 -0300

    [Map] Adds mapbox and dotenv as dependencies

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, Text} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import {MapView} from '@react-native-mapbox-gl/maps';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <Text>Empty Project</Text>
+      <SafeAreaView style={styles.container}>
+        <MapView
+          centerCoordinate={[-49.6446024, -27.2108001]}
+          preferedFramerPerSecond={60}
+          style={styles.container}
+          showUserLocation
+          zoom={3}
+        />
       </SafeAreaView>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
--- index.js
+++ index.js
@@ -5,5 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import MapboxGL from '@react-native-mapbox-gl/maps';
+import {MAPBOX_KEY} from '@env';
+
+MapboxGL.setAccessToken(MAPBOX_KEY);
 
 AppRegistry.registerComponent(appName, () => App);
